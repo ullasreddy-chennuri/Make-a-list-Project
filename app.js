4dcf4186683957266b6a3064b79b98e1e28023a5
ullasreddy-chennuri <ullasreddy3663@gmail.com>
2020-08-16 21:44:59 +0530

    Added Refresh Button

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const date = require(__dirname + '/date.js');
 
 const app = express();
 
-let items = ["Enter ur goal/Task","Sample-1"];
+let items = ["Enter ur goal/Task","Start Typing.."];
+
+// function refresh(){
+//     items = ["Enter ur goal/Task","Sample-1"];
+// }
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -26,6 +30,19 @@ app.post('/',function(req,res){
     res.redirect('/');
 })
 
+app.get('/refresh',function(req,res){
+    res.redirect('/');
+});
+
+app.post('/refresh',function(req,res){
+    items = ["Start Typing.."];
+    res.redirect('/');
+})
+
+
+
+
 app.listen(process.env.PORT || 3000 ,function(){
     console.log('Listening to the port 3000');
 });
+
