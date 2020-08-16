c321e3429495b88b5fe51519dd1ee41e487570d0
ullasreddy-chennuri <ullasreddy3663@gmail.com>
2020-08-16 21:10:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express =  require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
+
+const app = express();
+
+let items = ["Enter ur goal/Task","Sample-1"];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get('/',function(req,res){
+
+    let day = date();
+    res.render('list',{kindOfDay : day, newListItems : items });
+
+
+})
+
+app.post('/',function(req,res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+})
+
+app.listen(process.env.PORT || 3000 ,function(){
+    console.log('Listening to the port 3000');
+});
